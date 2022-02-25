19f7fabca75489abfc63eee794a96157e57d2ca8
Dmitry Mogilevtsev <webmogilevtsev@ya.ru>
2022-02-25 21:27:40 +0300

    add controller

--- backend/src/tariff/tariff.module.ts
+++ backend/src/tariff/tariff.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { TariffController } from './tariff.controller';
 import { TariffService } from './tariff.service';
 import { DbModule } from './../db/db.module';
 
 @Module({
     imports: [DbModule],
-    controllers: [],
+    controllers: [TariffController],
     providers: [TariffService],
 })
 export class TariffModule {}
