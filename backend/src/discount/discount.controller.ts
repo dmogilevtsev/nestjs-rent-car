a6a8b24d852be179e4602549ffec0bc0f1fbfcf6
Dmitry Mogilevtsev <webmogilevtsev@ya.ru>
2022-02-25 21:17:31 +0300

    change query name and add check input days with error hadlers

--- backend/src/discount/discount.controller.ts
+++ backend/src/discount/discount.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    HttpException,
+    HttpStatus,
+    Param,
+} from '@nestjs/common';
 
 import { IDiscount } from './discount.interface';
 import { DiscountService } from './discount.service';
 
+const MIN_COUNT_DAYS = 4;
+const MAX_COUNT_DAYS = 30;
 @Controller('discount')
 export class DiscountController {
     constructor(private readonly service: DiscountService) {}
 
-    @Get(':count-days')
-    async getOne(@Param('count-days') countDays: number): Promise<IDiscount> {
+    @Get(':count_days')
+    async getOne(@Param('count_days') countDays: number): Promise<IDiscount> {
+        if (countDays < MIN_COUNT_DAYS) {
+            throw new HttpException(
+                `No discount for a period less than ${MIN_COUNT_DAYS} days`,
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+        if (countDays > MAX_COUNT_DAYS) {
+            throw new HttpException(
+                `The rental period cannot exceed ${MAX_COUNT_DAYS} days`,
+                HttpStatus.BAD_REQUEST,
+            );
+        }
         return await this.service.getOneDiscount(countDays);
     }
 
