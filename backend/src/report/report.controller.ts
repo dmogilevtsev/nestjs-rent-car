5b35603e99d24fe39a2aa76a361d30274ac50d36
Dmitry Mogilevtsev <webmogilevtsev@ya.ru>
2022-02-25 20:40:41 +0300

    change inline value => constant var

--- backend/src/report/report.controller.ts
+++ backend/src/report/report.controller.ts
@@ -1,3 +1,4 @@
+import { MAX_DAY } from './../constants';
 import { ReportService } from './report.service';
 /*
 https://docs.nestjs.com/controllers#controllers
@@ -17,7 +18,7 @@ export class ReportController {
         @Res() res: Response,
         @Query() { dt_from, dt_to }: GetReportParamsDto,
     ): Promise<void> {
-        const dt1 = dt_from ? new Date(dt_from) : addDays(new Date(), -30);
+        const dt1 = dt_from ? new Date(dt_from) : addDays(new Date(), -MAX_DAY);
         const dt2 = dt_to ? new Date(dt_to) : new Date();
         const report = await this.reportService.report(dt1, dt2);
         res.send(report);
