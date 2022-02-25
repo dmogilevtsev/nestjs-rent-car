3eead24150b77694ffcd1709db8ac510a35280cb
Dmitry Mogilevtsev <webmogilevtsev@ya.ru>
2022-02-25 20:39:55 +0300

    add check input data and error handlers

--- backend/src/discount/discount.service.ts
+++ backend/src/discount/discount.service.ts
@@ -1,8 +1,10 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, HttpException, HttpStatus } from '@nestjs/common';
 
 import { DatabaseService } from './../db/database.service';
 import { IDiscount } from './discount.interface';
 
+const MIN_COUNT_DAYS = 3;
+const MAX_COUNT_DAYS = 30;
 @Injectable()
 export class DiscountService {
     private readonly log: Logger = new Logger(DiscountService.name);
@@ -19,6 +21,18 @@ export class DiscountService {
     }
 
     async getOneDiscount(countDays: number): Promise<IDiscount> {
+        if (countDays < MIN_COUNT_DAYS) {
+            throw new HttpException(
+                `No discount for a period less than ${MIN_COUNT_DAYS} days`,
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+        if (countDays > MAX_COUNT_DAYS) {
+            throw new HttpException(
+                `The rental period cannot exceed ${MAX_COUNT_DAYS} days`,
+                HttpStatus.BAD_REQUEST,
+            );
+        }
         try {
             const getDiscount = /* sql */ `select * from discount where $1 between day_from and day_to;`;
             const result = await this.db.executeQuery<IDiscount>(getDiscount, [
@@ -26,7 +40,7 @@ export class DiscountService {
             ]);
             return result[0];
         } catch (error) {
-            this.log.warn('[getAllDiscounts]', error);
+            this.log.warn('[getOneDiscount]', error);
         }
     }
 }
