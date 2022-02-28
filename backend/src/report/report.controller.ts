9163470b75f80a18c67b669fba87b321540ff49e
Dmitry Mogilevtsev <webmogilevtsev@ya.ru>
2022-02-28 15:16:30 +0300

    add check dates

--- backend/src/report/report.controller.ts
+++ backend/src/report/report.controller.ts
@@ -1,7 +1,14 @@
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Controller, Get, Res, Query } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Res,
+    Query,
+    HttpException,
+    HttpStatus,
+} from '@nestjs/common';
 import { Response } from 'express';
-import { addDays } from 'date-fns';
+import { addDays, isDate, isValid } from 'date-fns';
 
 import { MAX_DAY } from './../constants';
 import { ReportService } from './report.service';
@@ -21,6 +28,9 @@ export class ReportController {
         @Res() res: Response,
         @Query() { dt_from, dt_to, car_id }: GetReportParamsDto,
     ): Promise<void> {
+        if (!isValid(new Date(dt_from)) || !isValid(new Date(dt_to))) {
+            throw new HttpException('Incorrect date', HttpStatus.BAD_REQUEST);
+        }
         const dt1 = dt_from ? new Date(dt_from) : addDays(new Date(), -MAX_DAY);
         const dt2 = dt_to ? new Date(dt_to) : new Date();
         const report = await this.reportService.report(dt1, dt2, car_id);
