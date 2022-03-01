1a13e5d9e6f27fd29792aa17f95082fef9a1e8da
Dmitry Mogilevtsev <webmogilevtsev@ya.ru>
2022-03-01 13:53:58 +0300

    add e2e-test at car

---
+++ backend/test/car.e2e-spec.ts
@@ -0,0 +1,111 @@
+import { CreateSessionDto } from './../src/session/dto/create-session.dto';
+import { CreateCarDto } from './../src/car/dto/create-car.dto';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { Car } from '../src/car/entities/car.entity';
+import { ICar } from '../src/car/entities/car.interface';
+import { CarController } from '../src/car/controllers/car.controller';
+import { AppModule } from '../src/app.module';
+import { NestApplication } from '@nestjs/core';
+
+describe('[E2E Tests] CarController', () => {
+    let app: NestApplication;
+    let carController: CarController;
+    let createdCar: ICar;
+    beforeAll(async () => {
+        const moduleRef: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+        app = moduleRef.createNestApplication();
+        await app.init();
+        carController = moduleRef.get(CarController);
+    });
+
+    describe('getAllCars', () => {
+        let cars: ICar[];
+        beforeEach(async () => {
+            cars = await carController.getAllCars();
+        });
+        it('should be Car', () => {
+            expect(cars[0]).toMatchObject<ICar>(new Car());
+        });
+    });
+
+    describe('getOneCar', () => {
+        let car: ICar;
+        describe('with id = 1', () => {
+            beforeEach(async () => {
+                car = await carController.getOneCar(1);
+            });
+            it('should be not null', () => {
+                expect(car).not.toBeNull();
+            });
+            it('should be Car', () => {
+                expect(car).toMatchObject<ICar>(new Car());
+            });
+            it('id should be equal 1', () => {
+                expect(car?.id).toBe(1);
+            });
+        });
+        describe('with id = 2', () => {
+            beforeEach(async () => {
+                car = await carController.getOneCar(2);
+            });
+            it('should be not null', () => {
+                expect(car).not.toBeNull();
+            });
+            it('id should be equal 2', () => {
+                expect(car?.id).toBe(2);
+            });
+            it('id should be not equal 1', () => {
+                expect(car?.id).not.toBe(1);
+            });
+        });
+    });
+
+    describe('createCar', () => {
+        let carDto: CreateCarDto;
+        beforeEach(async () => {
+            carDto = {
+                brand: 'Niva',
+                model: 'Shevrolet',
+                gos: 'А321ББ123',
+                vin: '0987654321',
+            };
+            createdCar = await carController.createCar(carDto);
+        });
+        it('should be not null', () => {
+            expect(createdCar).not.toBeNull();
+        });
+        it('brand from new car should be equal brand from carDto', () => {
+            expect(createdCar?.brand).toBe(carDto.brand);
+        });
+        it('should be have ID', () => {
+            expect(createdCar.id).not.toBeNull();
+        });
+        it('id should be greater than 1', () => {
+            expect(createdCar.id).toBeGreaterThan(1);
+        });
+    });
+
+    describe('rentPrice', () => {
+        let sessioinDto: CreateSessionDto;
+        let rentPrice;
+        beforeEach(async () => {
+            sessioinDto = {
+                car_id: createdCar.id,
+                dt_from: new Date('2022-03-01'),
+                dt_to: new Date('2022-03-04'),
+                tariff_id: 2,
+            };
+            rentPrice = await carController.rentPrice(sessioinDto);
+        });
+        it('rentPrice should be 1254', () => {
+            expect(rentPrice).toBe(1254);
+        });
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+});
