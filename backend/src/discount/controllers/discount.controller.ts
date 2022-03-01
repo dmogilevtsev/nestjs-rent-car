420eafdc9f85ad1b497be8753a2c6985184d0b3d
Dmitry Mogilevtsev <webmogilevtsev@ya.ru>
2022-03-01 08:34:56 +0300

    create controller

---
+++ backend/src/discount/controllers/discount.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+
+import { IDiscount } from '../entities/discount.interface';
+import { DiscountService } from '../services/discount.service';
+
+@ApiTags('Discount Controller')
+@Controller('discount')
+export class DiscountController {
+    constructor(private readonly service: DiscountService) {}
+
+    @ApiResponse({
+        description: 'Get one discount',
+        status: 200,
+    })
+    @Get(':count_days')
+    async getOne(@Param('count_days') countDays: number): Promise<IDiscount> {
+        return await this.service.getOneDiscount(countDays);
+    }
+
+    @ApiResponse({
+        description: 'Get all discounts',
+        status: 200,
+    })
+    @Get()
+    async getAll(): Promise<IDiscount[]> {
+        return await this.service.getAllDiscounts();
+    }
+}
