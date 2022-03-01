8b0a41a7af86bdcfb50e13fd52600fb891020237
Dmitry Mogilevtsev <webmogilevtsev@ya.ru>
2022-03-01 08:28:27 +0300

    add report module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,18 @@
-import { ReportModule } from './report/report.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { DiscountModule } from './discount/discount.module';
+import { ReportModule } from './report/report.module';
 import { SessionModule } from './session/session.module';
 import { TariffModule } from './tariff/tariff.module';
 import { CarModule } from './car/car.module';
+import { DbModule } from './db/db.module';
 
 @Module({
     imports: [
-        ReportModule,
+        DbModule,
         ConfigModule.forRoot({ isGlobal: true, envFilePath: '../.env' }),
+        ReportModule,
         DiscountModule,
         SessionModule,
         TariffModule,
